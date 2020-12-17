29241b6d19b890392cea4860bf47a4ab664d4c7a
Swetha Ramesh <swetharamesh19@gmail.com>
2020-12-17 08:10:04 -0500

    rewrote cnmf.py, added setup.py, modified pbmc tutorial, updated readme

---
+++ cNMF/__init__.py
@@ -0,0 +1,11 @@
+from .cnmf import (
+    initialize,
+    prepare,
+    factorize,
+    combine,
+    consensus,
+    k_selection_plot,
+    get_usage_df,
+    get_gene_scores,
+    get_top_genes
+)
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='cNMF',
+                 version='0.1',
+                 description='Rewritten version of the cNMF analysis pipeline by Dylan Kotliar',
+                 url='',
+                 author='Swetha Ramesh',
+                 author_email='',
+                 license='',
+                 packages=setuptools.find_packages(),
+                 zip_safe=True)
